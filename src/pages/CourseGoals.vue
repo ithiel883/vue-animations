1b8d2e8d3c574eacf0e03439dd49d209460e0880
ithiel883 <ajuka.zephiniah@gmail.com>
2022-02-06 22:27:04 +0100

    initial file upload

---
+++ src/pages/CourseGoals.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class='container'>
+    <h2>Course Goals</h2>
+  <router-link to="/"> Users</router-link>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CourseGoals'
+};
+</script>
+
+<style scoped>
+
+</style>
